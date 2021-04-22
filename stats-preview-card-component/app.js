d4d64e23528278cce8ac26def58d8b3aad43f7a3
coreyhclay <coreyhclay@gmail.com>
2021-04-21 20:13:47 -0400

    formatting

--- stats-preview-card-component/app.js
+++ stats-preview-card-component/app.js
@@ -1,26 +1,25 @@
-wrapper = document.querySelector(".wrapper")
-card = document.querySelector(".card")
+wrapper = document.querySelector('.wrapper')
+card = document.querySelector('.card')
 
-wrapper.addEventListener("mousemove", function (e) {
-    x = e.clientX
-    y = e.clientY
+wrapper.addEventListener('mousemove', function (e) {
+  x = e.clientX
+  y = e.clientY
 
-    w = getComputedStyle(wrapper).width.slice(0,-2)
-    h = getComputedStyle(wrapper).height.slice(0,-2)
+  w = getComputedStyle(wrapper).width.slice(0, -2)
+  h = getComputedStyle(wrapper).height.slice(0, -2)
 
-    xPercent = (x / w) - 0.5
-    yPercent = (y / h) - 0.5
+  xPercent = x / w - 0.5
+  yPercent = y / h - 0.5
 
-    // console.log(xPercent, yPercent);
+  // console.log(xPercent, yPercent);
 
-    rotateY = xPercent * 30
-    rotateX = -(yPercent * 30)
+  rotateY = xPercent * 30
+  rotateX = -(yPercent * 30)
 
-
-    // console.log(rotateX, rotateY);
-    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+  // console.log(rotateX, rotateY);
+  card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
 })
 
-wrapper.addEventListener("mouseout", function () {
-    card.style.transform = "rotateX(0) rotateY(0)"
+wrapper.addEventListener('mouseout', function () {
+  card.style.transform = 'rotateX(0) rotateY(0)'
 })
